54b12d9ccdfdd9a5078aa40c1ffbc6889ac48c64
Qiya <120046278+hasnaqiyah@users.noreply.github.com>
2023-01-28 22:04:55 +0700

    Mengupload file js pertama kali
    menambahkan file dengan detail soal Homework W4

---
+++ HW4.js
@@ -0,0 +1,20 @@
+console.log("Homework-4")
+
+/* << Soal Homework W4 FSWD - Function pada Array Javascript >>
+Buatlah 100 nilai random (1 sampai 50) pada 1 array
+Pecahlah menjadi 2 array berdasarkan indexnya, yakni array pada index genap dan array pada index ganjil, gunakan method push() untuk menambahkan
+nilai baru pada array.
+
+Bandingkan kedua buah array, contoh
+- Min lebih besar array genap
+- Max lebih besar array ganjil
+- Total memiliki nilai sama antara array genap dan ganjil
+- Rata rata lebih besar array ganjil
+
+Output dari aplikasi:
+- Array dengan jumlah index 100
+- Array genap dengan jumlah index 50
+- Array ganjil dengan jumlah index 50
+- Min, Max, Total, Rata rata pada setiap array (genap dan ganjil)
+- Perbandingan nilai min, max, total dan rata rata */
+
